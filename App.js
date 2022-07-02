a8ba1325c9e2b2d2b4abd4a899f22e45c3f2f805
ANANYA KANNAN <104369167+velvet-facade@users.noreply.github.com>
2022-07-02 19:04:50 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,34 +1,34 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
-import LoginScreen from './screens/Login';
+import React, { Component } from "react";
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
+
+import BottomTabNavigator from "./components/BottomTabNavigator";
 import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false
+    };
+  }
 
-// pure javascript modules available in npm
-import { Card } from 'react-native-paper';
+  async loadFonts() {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
+    });
+    this.setState({ fontLoaded: true });
+  }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}></Text>
-      <LoginScreen />
-    </View>
-  );
+  componentDidMount() {
+    this.loadFonts();
+  }
+
+  render() {
+    const { fontLoaded } = this.state;
+    if (fontLoaded) {
+      return <BottomTabNavigator />;
+    }
+    return null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 5,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#FCF9C6',
-    padding: 8,
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
