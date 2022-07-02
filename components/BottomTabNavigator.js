6b68f8b24592265e153094e3df9db3d1ec653522
ANANYA KANNAN <104369167+velvet-facade@users.noreply.github.com>
2022-07-02 19:03:55 +0530

    Add files via upload

--- components/BottomTabNavigator.js
+++ components/BottomTabNavigator.js
@@ -4,60 +4,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
-import RideScreen from "../screens/Ride";
-import RideHistoryScreen from "../screens/RideHistory";
+//import login screen
+//import LoginScreen from './screens/Login'
 
 const Tab = createBottomTabNavigator();
 
-export default class BottomTabNavigator extends Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <Tab.Navigator
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
-
-              if (route.name === "Ride") {
-                iconName = "bicycle";
-              } else if (route.name === "Ride History") {
-                iconName = "time";
-              }
-
-              // You can return any component that you like here!
-              return (
-                <Ionicons
-                  name={iconName}
-                  size={size}
-                  color={color}
-                  size={size}
-                />
-              );
-            }
-          })}
-          tabBarOptions={{
-            activeTintColor: "#FBE5C0",
-            inactiveTintColor: "black",
-            style: {
-              height: 100,
-              borderTopWidth: 0,
-              backgroundColor: "#F88379"
-            },
-            labelStyle: {
-              fontSize: 20,
-              fontFamily: "Rajdhani_600SemiBold"
-            },
-            labelPosition: "below-icon",
-            tabStyle: {
-              alignItems: "center",
-              justifyContent: "center"
-            }
-          }}
-        >
-          <Tab.Screen name="Ride" component={RideScreen} />
-          <Tab.Screen name="Ride History" component={RideHistoryScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    );
+export default class BottomTabNavigator extends Component{
+  render(){
+    return(
+      console.log('hello world')
+    )
   }
 }
--- screens/Login.js
+++ screens/Login.js
@@ -11,12 +11,9 @@ import {
   KeyboardAvoidingView,
 } from 'react-native';
 import firebase from 'firebase';
+import { Card } from 'react-native-paper';
 
-
-  const bgImage = require('../assets/background1.png');
-  const appIcon = require('../assets/appIcon.png');
-  //const appName = require('../assets/appName.png'); 
-
+const broccoliImg = require('../assets/broccoli.png');
 
 export default class LoginScreen extends Component {
   constructor(props) {
@@ -27,109 +24,83 @@ export default class LoginScreen extends Component {
     };
   }
 
-  handleLogin = (email, password) => {
-    //Add code for firebase authentication
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(() => {
-        this.props.navigation.navigate('BottomTabNavigator');
-      })
-      .catch((error) => {
-        Alert.alert(error.message);
-      });
-  };
-
   render() {
-    const { email, password } = this.state;
     return (
-      <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <ImageBackground source={bgImage} style={styles.bgImage}>
-          <View style={styles.upperContainer}>
-            <Image source={appIcon} style={styles.appIcon} />
-            <Image source={appName} style={styles.appName} />
-          </View>
-          <View style={styles.lowerContainer}>
-            <TextInput
-              style={styles.textinput}
-              onChangeText={(text) => this.setState({ email: text })}
-              placeholder={'Enter Email'}
-              placeholderTextColor={'#FFFFFF'}
-              autoFocus
-            />
-            <TextInput
-              style={[styles.textinput, { marginTop: 20 }]}
-              onChangeText={(text) => this.setState({ password: text })}
-              placeholder={'Enter Password'}
-              placeholderTextColor={'#FFFFFF'}
-              secureTextEntry
-            />
-            <TouchableOpacity
-              style={[styles.button, { marginTop: 20 }]}
-              onPress={() => this.handleLogin(email, password)}>
-              <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-          </View>
-        </ImageBackground>
+      <KeyboardAvoidingView behavior="padding" style={{ marginTop: '4rem' }}>
+        <View>
+          <Text style={styles.appName}>AFresh Vegetables</Text>
+          <Text style={{ textAlign: 'center' }}>E-Grocery Market</Text>
+        </View>
+        <View style={styles.imgContainer}>
+          <Image source={broccoliImg} style={styles.broccoli} />
+        </View>
+        <View>
+          <TextInput
+            onChangeText={(text) => this.setState({ email: text })}
+            placeholder={'Enter Email'}
+            placeholderTextColor={'#0f5c10'}
+            autoFocus
+            style={styles.textInput}
+          />
+          <TextInput
+            onChangeText={(text) => this.setState({ password: text })}
+            placeholder={'Enter Password'}
+            placeholderTextColor={'#0f5c10'}
+            secureTextEntry
+            style={styles.textInput}
+          />
+        </View>
+        <View style={{ alignItems: 'center', marginTop: '1rem' }}>
+          <TouchableOpacity
+            style={styles.loginBtn}
+            onPress={
+              (console.log(this.state.email), 
+               console.log(this.state.password))
+            }>
+            <Text style={(styles.buttonText, { textAlign: 'center' })}>
+              Login
+            </Text>
+          </TouchableOpacity>
+        </View>
       </KeyboardAvoidingView>
     );
   }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFFFF',
-  },
-  bgImage: {
-    flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
-  },
-
-  upperContainer: {
-    flex: 0.5,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  appIcon: {
-    width: 280,
-    height: 280,
-    resizeMode: 'contain',
-    marginTop: 80,
-  },
-  appName: {
-    width: 130,
-    height: 130,
-    resizeMode: 'contain',
-  },
-  lowerContainer: {
-    flex: 0.5,
-    alignItems: 'center',
-  },
-  textinput: {
-    width: '75%',
-    height: 55,
-    padding: 10,
-    borderColor: '#FFFFFF',
-    borderWidth: 4,
-    borderRadius: 10,
-    fontSize: 18,
-    color: '#FFFFFF',
-    fontFamily: 'Rajdhani_600SemiBold',
-    backgroundColor: '#5653D4',
-  },
-  button: {
-    width: '43%',
-    height: 55,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F48D20',
-    borderRadius: 15,
-  },
   buttonText: {
     fontSize: 24,
     color: '#FFFFFF',
-    fontFamily: 'Rajdhani_600SemiBold',
+  },
+  imgContainer: {
+    alignItems: 'center',
+  },
+  broccoli: {
+    width: 150,
+    height: 150,
+    marginTop: '-3rem',
+  },
+  loginBtn: {
+    width: '5rem',
+    height: '2.5rem',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E0DECA',
+    borderRadius: 5,
+   // border: 'solid',
+    borderWidth: 0.1,
+  },
+  appName: {
+    marginTop: -100,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  textInput: {
+    backgroundColor: '#C7D36F',
+    height: '2rem',
+    textAlign: 'center',
+    borderRadius: 4,
+    borderWidth: 1,
+    marginTop: '1rem',
   },
 });
